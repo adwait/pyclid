8725819b90be8469e64a7c8b8bca7c5b354159b0
adwait <godbole15@gmail.com>
2024-02-10 22:18:07 -0800

    Working version

--- test.py
+++ test.py
@@ -8,15 +8,15 @@ inttype = UclidIntegerType()
 booltype = UclidBooleanType()
 
 class Test(unittest.TestCase):
-    def makeModule(self):
-        return UclidModule("main")
-    
+
     def testAdd(self):
-        m = self.makeModule()
+        m = UclidModule("main")
         a = m.mkVar("a", inttype)
         b = m.mkVar("b", inttype)
         c = m.mkVar("c", inttype)
-        init = UclidInit([
+        d = m.mkVar("d", inttype, PortType.output)
+        e = m.mkVar("e", inttype, PortType.output)
+        init = UclidInitBlock([
             UclidAssignStmt(a, UclidLiteral(1)),
             UclidAssignStmt(b, UclidLiteral(2)),
             UclidAssignStmt(c, a + b)
@@ -25,12 +25,12 @@ class Test(unittest.TestCase):
         self.assertTrue('var c : integer;' in m.__inject__())
     
     def testUninterpretedType(self):
-        m = self.makeModule()
+        m = UclidModule("main")
         t = m.mkUninterpretedType("mytype")
         a = m.mkVar("a", inttype)
         b = m.mkVar("b", inttype)
         c = m.mkVar("c", t)
-        init = UclidInit([
+        init = UclidInitBlock([
             UclidAssignStmt(a, UclidLiteral(1)),
             UclidAssignStmt(b, UclidLiteral(2)),
             UclidAssignStmt(c, a + b)
@@ -39,13 +39,13 @@ class Test(unittest.TestCase):
         self.assertTrue('var c : mytype;' in m.__inject__())
 
     def testArrayType(self):
-        m = self.makeModule()
+        m = UclidModule("main")
         t = m.mkArrayType(inttype, inttype)
         s = m.mkArrayType("myarr", inttype, inttype)
         a = m.mkVar("a", inttype)
         b = m.mkVar("b", inttype)
         c = m.mkVar("c", s)
-        init = UclidInit([
+        init = UclidInitBlock([
             UclidAssignStmt(a, UclidLiteral(1)),
             UclidAssignStmt(b, UclidLiteral(2)),
             UclidAssignStmt(c, a + b)
@@ -54,12 +54,12 @@ class Test(unittest.TestCase):
         self.assertTrue('var c : myarr;' in m.__inject__())
 
     def testConst(self):
-        m = self.makeModule()
+        m = UclidModule("main")
         a = m.mkVar("a", inttype)
         b = m.mkVar("b", inttype)
         c = m.mkVar("c", inttype)
         d = m.mkConst("d", inttype, UclidLiteral(3))
-        init = UclidInit([
+        init = UclidInitBlock([
             UclidAssignStmt(a, UclidLiteral(1)),
             UclidAssignStmt(b, UclidLiteral(2)),
             UclidAssignStmt(c, a + b),
@@ -67,6 +67,43 @@ class Test(unittest.TestCase):
         ])
         m.setInit(init)
         self.assertTrue('const d : integer = 3;' in m.__inject__())
+
+    def testBig(self):
+        m1 = UclidModule("subm")
+        m1.mkVar("a", inttype)
+        
+        t1 = m1.mkUninterpretedType("mytype")
+        t2 = m1.mkArrayType("arr_t", inttype, inttype)
+        v1 = m1.mkVar("v1", t1)
+        v2 = m1.mkVar("v2", t2)
+        init1 = UclidInitBlock([
+            UclidAssignStmt(UclidArraySelect(v2, [UclidLiteral(1)]), UclidLiteral(1)),
+            UclidHavocStmt(v1)
+        ])
+        m1.setInit(init1)
+
+        m2 = UclidModule("main")
+        a = m2.mkVar("a", inttype)
+        b = m2.mkVar("b", inttype)
+
+        m2.mkImport(DeclTypes.TYPE, "t1", m1, "mytype")
+        m2.setInit(UclidInitBlock([
+            UclidAssignStmt(a, UclidLiteral(1)),
+            UclidAssignStmt(b, UclidLiteral(2)),
+            UclidAssumeStmt(UclidOpExpr("eq", [a, b]))
+        ]))
+        m2.setNext(UclidNextBlock([
+            UclidAssignStmt(a.p(), UclidOpExpr("add", [a, b])),
+            UclidAssignStmt(b.p(), UclidOpExpr("add", [a, UclidLiteral("1")])),
+        ]))
+        m2.setControl(UclidControlBlock([
+            UclidBMCCommand("v", 10),
+            UclidPrintCexJSONCommand("v"),
+            UclidPrintResultsCommand()
+        ]))
+
+        print(m1.__inject__())
+        print(m2.__inject__())
 
     
 if __name__ == '__main__':
